57cecdf8fbdb314e79d04d010b57981d9e034e60
shandawei <shandawei@me.com>
2022-05-17 00:32:34 +0800

    添加单元测试

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,2 +1,23 @@
 import { } from 'jest';
 import { IpcRendererWorker, IpcRenderer } from '../src/index';
+
+class MockYzbNative {
+    setCallback() { }
+    sendProcessMessage() { }
+}
+(global as any).yzb = {
+    native: new MockYzbNative()
+};
+
+describe('IpcRendererWorker check', () => {
+    test('check constructor', () => {
+
+    });
+});
+
+
+describe('IpcRenderer check', () => {
+    test('check constructor', () => {
+
+    });
+});
